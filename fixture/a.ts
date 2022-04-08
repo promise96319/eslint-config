7c716d469ba4615dbdb0c3fef8134ae24b9d1a2e
promise96319 <1248975357@qq.com>
2022-04-09 00:20:06 +0800

    feat: add typescript config

--- fixture/a.ts
+++ fixture/a.ts
@@ -1,3 +1,10 @@
 const a = {
   B: 1,
 }
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface A { a: string }
---
+++ packages/all/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@qgh/eslint-config-ts',
+  ],
+}
